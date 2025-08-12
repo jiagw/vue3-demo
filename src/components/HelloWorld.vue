e2de50f7506d76008f14c1166357ee4385b3fa47
jgw <656663782@qq.com>
2025-08-12 17:39:53 +0800

    333

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -15,7 +15,7 @@
     <ul>
       <li><a href="https://vuejs.org" target="_blank" rel="noopener">111</a></li>
       <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">222</a></li>
-      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
+      <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">333</a></li>
 <!--      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>-->
       <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
     </ul>
