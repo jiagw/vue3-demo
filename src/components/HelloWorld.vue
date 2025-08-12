92ada0f2fc24055b2399853bc6ed2acc15d886e6
jgw <656663782@qq.com>
2025-08-12 17:34:57 +0800

    2222

--- src/components/HelloWorld.vue
+++ src/components/HelloWorld.vue
@@ -14,7 +14,7 @@
     <h3>Essential Links</h3>
     <ul>
       <li><a href="https://vuejs.org" target="_blank" rel="noopener">111</a></li>
-      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">Forum</a></li>
+      <li><a href="https://forum.vuejs.org" target="_blank" rel="noopener">222</a></li>
       <li><a href="https://chat.vuejs.org" target="_blank" rel="noopener">Community Chat</a></li>
 <!--      <li><a href="https://twitter.com/vuejs" target="_blank" rel="noopener">Twitter</a></li>-->
       <li><a href="https://news.vuejs.org" target="_blank" rel="noopener">News</a></li>
